dde8fc22bb4ff08c05ed66ff2de617a3ee70c0d6
Pavan_Kumar <63488643+pavan6476252@users.noreply.github.com>
2023-03-26 09:26:04 +0000

    basic view finished

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
+
 const app = express();
+
+const key = "2d7dcf1938de40ba82a915a17aaf3d9a";
+const baseUrl = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${key}`;
 
 app.set("view engine", "ejs");
 
-app.get('/',function (req,res){
-    res.render("index");
-})
+app.get("/", function (req, res) {
+  axios
+    .get(baseUrl)
+    .then(function (response) {
+      let articles = response.data.articles;
+
+      res.render("index",{articles:articles});
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {});
+
+});
+
+app.listen(3000, function () {
+  console.log("listening 3000");
+});
